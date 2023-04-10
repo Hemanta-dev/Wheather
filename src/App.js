edc30cc6b25c199d2711b34f1a3ae9c7906a492e
Hemanta-dev <www.hemantaadhikari@gmail.com>
2023-04-10 09:23:49 +0545

    first commit for wheather app based on react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import Header from "./componet/Header";
+import Temp from "./componet/Temp";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="body">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componet/Header.jsx
@@ -0,0 +1,9 @@
+const Header =() =>{
+    return (
+        <header>
+            <h1>Let's See Your Location Temparatue </h1>
+        </header>
+       
+    )
+}
+export default Header;
